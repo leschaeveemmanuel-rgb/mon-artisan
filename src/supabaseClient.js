4cee6849a117b98563df7517455c67e1d90792e9
Emmanuel LESCHAEVE <leschaeve.emmanuel@gmail.com>
2026-02-16 15:42:06 +0100

    Add files via upload

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Ces informations sont disponibles sur votre tableau de bord Supabase
-const supabaseUrl = 'https://kteaebstdgeqlxhuidaz.supabase.co'
-const supabaseAnonKey = 'sb_publishable_aEGE5GTu4nTWaR39lQp4SQ_77DJkzMg'
+const supabaseUrl ='https://kteaebstdgeqlxhuidaz.supabase.co'
+const supabaseAnonKey ='sb_publishable_aEGE5GTu4nTWaR39lQp4SQ_77DJkzMg'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
